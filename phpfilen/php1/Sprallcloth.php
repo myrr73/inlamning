b58643fea28ffed8fe5bef66c1ecf2f3bf1d1589
Tobbe Myrander <tobias@myrander.se>
2019-03-07 09:31:04 +0100

    phpfilen

---
+++ phpfilen/php1/Sprallcloth.php
@@ -0,0 +1,176 @@
+<?php 
+
+$jsonData = new stdClass();
+$item = array (
+
+            array(
+                'id' => 001,
+                'name' => 'Angry Cupcake Body',
+                'image' => 'https://lh3.googleusercontent.com/2X5wmOh95h4lZyHbtHXBmwDiQDzyyV6-ZhSMXCriSWPJpaJEW1aOIWpkE7As1Z_-FsOvPYTzGxKuTXZB7MRS=w1366-h657-rw',
+                'size' => '0-3m',
+                'info' => 'Angry Cupcake',
+                'color' => 'Light yellow',
+                'price' => 149,
+            ),
+
+            array(
+                'id' => 002,
+                'name' => 'Happy Donut T-shirt',
+                'image' => 'https://lh4.googleusercontent.com/BS6Ze_yaQFD0iAssIyhP0Vy0Go8diDfrfYHAZmFBck-BUOkuC_y8CkZhpEgAuOWx3g4eZHyF_7f_odVIEfz1=w1366-h657-rw',
+                'size' => '0-3m',
+                'info' => 'Happy Donut',
+                'color' => 'Light yellow',
+                'price' => 149,
+            ),
+
+            array(
+                'id' => 003,
+                'name' => 'Angry Cupcake Body',
+                'image' => 'https://lh3.googleusercontent.com/tPYi1PK2b20EYqQtW5hP1BaIbteSQnOVIAEXGXvPL3ZZPZ3UHhRR7El7bWPD0xujPho6melD7Iu8XVDRHbLv=w1366-h657-rw',
+                'size' => '4-6m',
+                'info' => 'Angry Cupcake',
+                'color' => 'Light grey',
+                'price' => 169,
+            ),
+
+            array(
+                'id' => 004,
+                'name' => 'Happy Donut T-shirt',
+                'image' => 'https://lh4.googleusercontent.com/FmCPz9NSYzRuhQHdsrKPgkhOXyyCJjeN6zRuU1Oj2uVQ94At1hxwvkyXgZR9tW_GD-9E1I3K94sHypdDJshH=w1366-h657-rw',
+                'size' => '4-6m',
+                'info' => 'Happy Donut',
+                'color' => 'Light grey',
+                'price' => 169,
+            ),
+
+            array(
+                'id' => 005,
+                'name' => 'Angry Cupcake Body',
+                'image' => 'https://lh5.googleusercontent.com/L_8PU1JXjWd-VBn3KHPYZOLU-MJ4I7H3twjc8_fNJVkst19gnP3GNRE05yXC3r3pV8OLTBucxPcgagTeCcWQ=w1366-h657-rw',
+                'size' => '7-9m',
+                'info' => 'Angry Cupcake',
+                'color' => 'Light pink',
+                'price' => 179,
+            ),
+
+            array(
+                'id' => 006,
+                'name' => 'Happy Donut T-shirt',
+                'image' => 'https://lh4.googleusercontent.com/qEQ0p0HKmwCpSuZEN3n7Kd1Vjp-RhOjc4Ny0lh4RAxuVPFQdfc9gIBbB3M_HtK0ytnZ13OriIVrY48MX812W=w1366-h657-rw',
+                'size' => '7-9m',
+                'info' => 'Angry Cupcake',
+                'color' => 'Light pink',
+                'price' => 179,
+            ),
+
+            array(
+                'id' => 007,
+                'name' => 'Angry Cupcake Body',
+                'image' => 'https://lh5.googleusercontent.com/8vrgvr_k7QbKKzXVzSpmCLazeoMIFjsSgcuiKZzH8GFNWK8sQJnAvPf0cB-JAmSFrNQmmC7k5DJF-Tge6Kac=w1366-h657-rw',
+                'size' => '10-12m',
+                'info' => 'Angry Cupcake',
+                'color' => 'Light brown',
+                'price' => 199,
+            ),
+
+            array(
+                'id' => 008,
+                'name' => 'Happy Donut T-shirt',
+                'image' => 'https://lh4.googleusercontent.com/UyGKmKdKbrZhhGNEDGHraY_YKYQ4Ufo3QlCapPYmVT7TxiyiGNg4e6t77TlY_07eOzpMDKdidn6B9w-Mu4xS=w1366-h657-rw',
+                'size' => '10-12',
+                'info' => 'Angry Cupcake',
+                'color' => 'Light brown',
+                'price' => 199,
+            ),
+
+            array(
+                'id' => 009,
+                'name' => 'Angry Cupcake Body',
+                'image' => 'https://lh6.googleusercontent.com/pafGulFHRiM1qY8ai1J6OlEBF7nOfGhb9IEpAqQivnLXkMxaBw7ETqfylUBUzTb7STKJA2nIbQpTdjp3hkK8=w1366-h657-rw',
+                'size' => '12-18m',
+                'info' => 'Angry Cupcake',
+                'color' => 'Light corall',
+                'price' => 239,
+            ),
+
+            array(
+                'id' => 010,
+                'name' => 'Happy Donut T-shirt',
+                'image' => 'https://lh4.googleusercontent.com/3e8KnNEZQRhbJqLwRrsX_FRKhEIwNy4nEQ_jhkjHnbfz6szETovHCiT6STtRuY_uRtqRevZupszTo3jVi3x5=w1366-h657-rw',
+                'size' => '12-18m',
+                'info' => 'Happy Donut',
+                'color' => 'Light corall',
+                'price' => 239,
+            ),
+
+            array(
+                'id' => 011,
+                'name' => 'Angry Cupcake Body',
+                'image' => 'https://lh5.googleusercontent.com/iwPKGiivC71n2evKVP-D3kRDbxhhUwTGhagZMGvBcrTiTqAyj8Bfbol3nenZVJ6-kHaF64gbgciIVTTz7uB1=w1366-h657-rw',
+                'size' => '1½-2',
+                'info' => 'Angry Cupcake',
+                'color' => 'Light blue',
+                'price' => 259,
+            ),
+
+            array(
+                'id' => 012,
+                'name' => 'Happy Donut T-shirt',
+                'image' => 'https://lh6.googleusercontent.com/j2ORbkaRvwO01vVALXbSjYX0ytkCt0W18ah24yUb7c48QSBTwQXIFshCaHpNqlzz6KjN4uT_AwYv5CMX48IX=w1366-h657-rw',
+                'size' => '1½-2',
+                'info' => 'Happy Donut',
+                'color' => 'Light blue',
+                'price' => 259,
+            ),
+
+            array(
+                'id' => 013,
+                'name' => 'Angry Cupcake Body',
+                'image' => 'https://lh3.googleusercontent.com/Zx8ZtvK8MkN1ynSqQDnxUD4Jf42zMwi9KlDAKxhv2-OWTarg49eRWfaZiDxpCPVBTi2FSvjiwv1P2CXzwEcE=w1366-h657-rw',
+                'size' => '2-3',
+                'info' => 'Angry Cupcake',
+                'color' => 'Light green',
+                'price' => 299,
+            ),
+
+            array(
+                'id' => 014,
+                'name' => 'Happy Donut T-shirt',
+                'image' => 'https://lh5.googleusercontent.com/p_TBOjOo-U4Msy_CsxkCsyEFTjM5KbID1A5igcfhAmp3C3zUy8aHMLpPtT7vGoSF4NesE3M5y_vAuJrSJ66j=w1366-h657-rw',
+                'size' => '2-3',
+                'info' => 'Happy Donut',
+                'color' => 'Light green',
+                'price' => 299,
+            ),
+
+            array(
+                'id' => 015,
+                'name' => 'Angry Cupcake Body',
+                'image' => 'https://lh3.googleusercontent.com/Lhps85H8B5jS1zPil9yYLvBIzlsPWlbzmE7HAR2WF1xHsgHkNuUBZBZ0xjLJBvAd3iaeKBrXtJCPRRRQdAcc=w1366-h657-rw
+                ',
+                'size' => '4-6',
+                'info' => 'Angry Cupcake',
+                'color' => 'Light red',
+                'price' => 299,
+            ),
+
+            array(
+                'id' => 016,
+                'name' => 'Happy Donut T-shirt',
+                'image' => 'https://lh5.googleusercontent.com/vYIpcOkBWw0YzD4Z2DfyvKY0W0pAIovTQH1T6PR076ZQB3DBWnw9bgXtGYnHpeTucjeb34AHs0nipkMNDgBi=w1366-h657-rw',
+                'size' => '4-6',
+                'info' => 'Angry Cupcake',
+                'color' => 'Light red',
+                'price' => 299,
+            ),
+        );
+
+        $jsonItem = json_encode($item);
+
+        header('Access-Control-Allow-Origin: *');
+
+        echo $jsonItem;
+
+        ?>
+        
+        
